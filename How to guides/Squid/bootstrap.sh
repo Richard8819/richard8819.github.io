accdb190deeb09ca14d25e4eb61ba880cb14fe84
Richard8819 <richardsonjos@gmail.com>
2026-04-23 17:43:38 +0530

    Create bootstrap.sh for Squid Proxy setup
    Added a bootstrap script for Squid Proxy installation and configuration, including repository setup and logging.

--- How to guides/Squid/bootstrap.sh
+++ How to guides/Squid/bootstrap.sh
@@ -1,1 +1,42 @@
+#!/bin/bash
+echo "=============================================="
+echo "       Squid-Proxy Bootstrap-version:0.2"     "
+echo "=============================================="
 
+#Def pkg name
+package_name='squid'
+#Def squid user
+SQUID_USER='squidadm'
+#Def log file and path
+LOG_FILE="/var/log/squid_install.log"
+echo > $LOG_FILE
+
+# Function to log messages
+log_message()  {
+  echo "$(date '+%Y-%m-%d %H:%M:%S' ) - $1" | tee -a "$LOG_FILE"
+}
+  log_message "Starting Squid Proxy installation/configuration..."
+
+# Function to check scb-webservices.repo exist or not in yum.repo.d repository
+
+if [[ -s "/etc/yum.repos.d/scb-webservices.repo" ]]; then
+  log_message "scb-webservices.repo already exists. Skipping repository setup."
+else
+  log_message "scb-webservices.repo not found. Setting up repository..."
+  
+  sudo tee /etc/yum.repos.d/scb-webservices.repo > /dev/null <<EOL
+[scb-webservices]
+name=SCB Webservices Repository
+baseurl=https://artifactory.global.standardchartered.com/artifactory/rpm-webeng-production_local/webrpm/RHEL9
+enabled=1
+gpgcheck=1
+gpgkey=https://artifactory.global.standardchartered.com/artifactory/rpm-webeng-production_local/webrpm/RHEL9/SCB-WEBENG-RPM-KEY
+EOL
+
+
+# Update package repository
+  sudo yum update -y > dev/null
+mkdir -p /etc/squid/src-workload-configs
+touch /etc/squid/src-workload-configs/dummy.conf
+ip= ifconfig ens192 | grep inet | awk '{print $2}'
+echo "acl src_42030 src $ip" > /etc/squid/src-workload-configs/dummy.conf
